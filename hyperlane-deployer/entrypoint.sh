15af1e97e66bbec9b09dd1cdd297505ce08f15f0
Shawn <44221603+shaspitz@users.noreply.github.com>
2023-11-13 03:33:20 -0800

    warp route works

--- hyperlane-deployer/entrypoint.sh
+++ hyperlane-deployer/entrypoint.sh
@@ -3,7 +3,7 @@ set -exu
 
 sleep 5
 
-if test -f /deploy-artifacts/done; then
+if test -f artifacts/done; then
   echo "Deploy artifacts already exist. Skipping deployment."
 else
   echo "Deploy artifacts do not exist. Deploying."
@@ -11,20 +11,29 @@ else
     --remotes goerli \
     --key $CONTRACT_DEPLOYER_PRIVATE_KEY
 
-  # COPY addresses.json and agent_config.json to deploy artifacts volume
-  cp artifacts/addresses.json /deploy-artifacts/addresses.json
-  cp artifacts/agent_config.json /deploy-artifacts/agent_config.json
-
   # Signal done 
-  touch /deploy-artifacts/done
+  touch artifacts/done
 fi
 
-# Shim to run test script, move file to where their script expects it
-mkdir -p ./artifacts
-cp /deploy-artifacts/addresses.json ./artifacts/addresses.json
+# DEBUG=hyperlane* yarn ts-node scripts/test-messages.ts \
+#   --chains goerli mevcommitsettlement \
+#   --key $CONTRACT_DEPLOYER_PRIVATE_KEY
 
-DEBUG=hyperlane* yarn ts-node scripts/test-messages.ts \
-  --chains goerli mevcommitsettlement \
+# TODO: may need to specify owner and other addrs
+
+if test -f artifacts/done-warp-route; then
+  echo "Warp route already deployed. Skipping."
+else
+  echo "Deploying warp route."
+  DEBUG=hyperlane* yarn ts-node scripts/deploy-warp-routes.ts \
+    --key $CONTRACT_DEPLOYER_PRIVATE_KEY
+
+  touch artifacts/done-warp-route
+fi
+
+yarn ts-node scripts/test-warp-transfer.ts \
+  --origin goerli --destination mevcommitsettlement --wei 1 \
+  --recipient 0xdCfaD16AF20410D24757d0ae9366e122B55Af405 \
   --key $CONTRACT_DEPLOYER_PRIVATE_KEY
 
 # Infinite loop
