364f87b934efec46474d75c08c20c13376d60ee5
Shawn <44221603+shaspitz@users.noreply.github.com>
2023-11-17 10:54:06 -0800

    rm

--- util/swap-contract/contracts/Token.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.8.19;
-
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
-contract Token is ERC20 {
-    constructor() ERC20("MyToken", "MTK") {
-        _mint(msg.sender, 1000 * (10 ** uint256(decimals())));
-    }
-}
